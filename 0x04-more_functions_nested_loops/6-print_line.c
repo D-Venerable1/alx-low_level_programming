61dc3a6c3c1ed717b1ca500599ff2fdd830ef437
Lawal Fadhlullah <lawalfadhlullah74@gmail.com>
2023-02-23 22:02:51 -0800

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - Draws a straight line of underscores in the terminal.
+ *
+ * Parameters:
+ * n: The number of underscores to print in the line.
+ *
+ * putchar - Prints a single character to the standard output stream.
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		putchar('\n');
+		return;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
