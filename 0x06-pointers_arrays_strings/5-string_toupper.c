6062b88eb2df99f41b194acc03181cb823e37b69
Lawal Fadhlullah <lawalfadhlullah74@gmail.com>
2023-03-02 00:47:53 -0800

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase letters of a string to uppercase
+ *
+ * @str: string to modify
+ *
+ * Return: pointer to modified string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+	}
+
+	return (str);
+}
