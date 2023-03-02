2f4cf1554504058dfac71039e3568d18052a5f1a
Lawal Fadhlullah <lawalfadhlullah74@gmail.com>
2023-03-02 01:36:22 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encode a string into 1337
+ *
+ * @str: string to encode
+ *
+ * Return: pointer to encoded string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char leet[] = "4433007711";
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; letters[j]; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = leet[j];
+				break;
+			}
+		}
+
+	}
+
+	return (str);
+}
