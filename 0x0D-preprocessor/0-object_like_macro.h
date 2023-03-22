8f64ae2a4d27c4b2fe16fac9550e6acb6c322d72
Lawal Fadhlullah <lawalfadhlullah74@gmail.com>
2023-03-21 17:41:18 -0700

    preprocessor task o

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * 0-object_like_macro.h
+ *
+ * Defines macros for commonly used values.
+ */
+
+#define SIZE 1024 /* Abbreviation for 1024 */
+
+#endif 
